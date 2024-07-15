5acdb48606af518f35723387d4cb0457a5d4ab3f
davee8k <davee8k@users.noreply.github.com>
2024-07-15 18:25:33 +0200

    Code reformat

--- src/SlimPlate/SlimPlate.php
+++ src/SlimPlate/SlimPlate.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace SlimPlate;
 
@@ -16,9 +17,12 @@ use InvalidArgumentException;
  * @license https://unlicense.org/
  * @version 0.87.2
  */
-class SlimPlate {
+class SlimPlate
+{
 	/** @var string */
 	protected static $regexIf = '/\{if((?:(?!\{if).)*)\}((?:(?!\{if).)+)\{\/if\}/iU';
+	/** @var string */
+	protected static $regexCond = '\$([a-z][a-z\d_]*)(?:(?:->|-&gt;)([a-z][a-z\d_]*))';
 	/** @var string */
 	protected $template = '';
 	/** @var array<int, array<int, string>> */
@@ -28,7 +32,8 @@ class SlimPlate {
 	 * SlimPlate constructor
 	 * @param string $template
 	 */
-	public function __construct (string $template) {
+	public function __construct(string $template)
+	{
 		$this->template = $template;
 	}
 
@@ -39,13 +44,14 @@ class SlimPlate {
 	 * @return true
 	 * @throws InvalidArgumentException
 	 */
-	public function check (array $data = [], bool $strict = false): bool {
+	public function check(array $data = [], bool $strict = false): bool
+	{
 		$text = $this->template;
 		while ($text && preg_match_all(self::$regexIf, $text, $matches)) {
 			if (!empty($matches[0])) {
-				foreach ($matches[0] as $i=>$match) {
+				foreach ($matches[0] as $i => $match) {
 					if (!$text) break;
-					$text = preg_replace('/'.preg_quote($match,'/').'/', '', $text, 1);
+					$text = preg_replace('/'.preg_quote($match, '/').'/', '', $text, 1);
 					$this->checkCondition($data, $matches[1][$i], $matches[2][$i]);
 				}
 			}
@@ -58,19 +64,19 @@ class SlimPlate {
 	 * @param mixed[] $data
 	 * @return string
 	 */
-	public function render (array $data = []): string {
+	public function render(array $data = []): string
+	{
 		$text = $this->template;
 		while ($text && preg_match_all(self::$regexIf, $text, $matches)) {
 			if (!empty($matches[0])) {
-				foreach ($matches[0] as $i=>$match) {
+				foreach ($matches[0] as $i => $match) {
 					if (!$text) break;
 					try {
 						$text = $this->convertCondition($text, $data, $match, $matches[1][$i], $matches[2][$i]);
-					}
-					catch (InvalidArgumentException $e) {
+					} catch (InvalidArgumentException $e) {
 						// ignore errors
 					}
-					$text = preg_replace('/'.preg_quote($match,'/').'/', '&#'.ord('{').';'.substr($match, 1, -1).'&#'.ord('}').';', $text, 1);
+					$text = preg_replace('/'.preg_quote($match, '/').'/', '&#'.ord('{').';'.substr($match, 1, -1).'&#'.ord('}').';', $text, 1);
 				}
 			}
 		}
@@ -88,19 +94,25 @@ class SlimPlate {
 	 * @param string $fullCon
 	 * @throws InvalidArgumentException
 	 */
-	protected function checkCondition (array $data, string $condition, string $fullCon): void {
+	protected function checkCondition(array $data, string $condition, string $fullCon): void
+	{
+		if (preg_match_all('/{else}/i', $fullCon) > 1) {
+			throw new InvalidArgumentException("Multiple else in: ".$fullCon);
+		}
+
 		$con = trim($condition);
-
-		if (preg_match_all('/{else}/i', $fullCon) > 1) throw new InvalidArgumentException("Multiple else in: ".$fullCon);
-
 		$val = $this->parseUknown($data, $con);
 		if ($val) $con = $this->removeRegex($con, (string) $val[0]);
 		else throw new InvalidArgumentException('Empty IF condition');
 
 		if ($con) {
 			$operator = $this->findOperator($con);
-			if ($operator && $this->operator(0, $operator[0], 0) !== null) $con = $this->removeRegex($con, $operator[0]);
-			else throw new InvalidArgumentException("Undefined IF operator: ".$con);
+			if ($operator && $this->operator(0, $operator[0], 0) !== null) {
+				$con = $this->removeRegex($con, $operator[0]);
+			}
+			else {
+				throw new InvalidArgumentException("Undefined IF operator: ".$con);
+			}
 
 			$val = $this->parseUknown($data, $con);
 			if (!$val) throw new InvalidArgumentException('Missing second variable: '.trim($condition));
@@ -116,7 +128,8 @@ class SlimPlate {
 	 * @param string $fullCon
 	 * @return string
 	 */
-	protected function convertCondition (string $text, array $data, string $match, string $condition, string $fullCon): string {
+	protected function convertCondition(string $text, array $data, string $match, string $condition, string $fullCon): string
+	{
 		$con = trim($condition);
 		$firstVal = $this->parseUknown($data, $con);
 		if ($firstVal) {
@@ -133,13 +146,12 @@ class SlimPlate {
 
 						$show = $this->operator($firstVal[1], $operator[0], $secVal[1]);
 						if (!$con && $show !== null) {
-							$text = preg_replace('/'.preg_quote($match,'/').'/', $this->decideIfElse($show, $fullCon), $text, 1);
+							$text = preg_replace('/'.preg_quote($match, '/').'/', $this->decideIfElse($show, $fullCon), $text, 1);
 						}
 					}
 				}
-			}
-			else {
-				$text = preg_replace('/'.preg_quote($match,'/').'/', $this->decideIfElse(boolval($firstVal[1]), $fullCon), $text, 1);
+			} else {
+				$text = preg_replace('/'.preg_quote($match, '/').'/', $this->decideIfElse(boolval($firstVal[1]), $fullCon), $text, 1);
 			}
 		}
 		return $text ?? '';
@@ -152,19 +164,23 @@ class SlimPlate {
 	 * @return true
 	 * @throws InvalidArgumentException
 	 */
-	protected function checkVariables (array $data, bool $strict = false): bool {
+	protected function checkVariables(array $data, bool $strict = false): bool
+	{
 		$notFound = $data;
 		foreach ($this->matchVariables() as $match) {
 			if (count($match) === 2 && isset($notFound[$match[1]])) {
 				unset($notFound[$match[1]]);
-			}
-			else if (count($match) === 3 && isset($notFound[$match[1]][$match[2]])) {
+			} elseif (count($match) === 3 && isset($notFound[$match[1]][$match[2]])) {
 				unset($notFound[$match[1]][$match[2]]);
 				if (empty($notFound[$match[1]])) unset($notFound[$match[1]]);
+			} else {
+				throw new InvalidArgumentException("Unset variable: ".$match[0]);
 			}
-			else throw new InvalidArgumentException("Unset variable: ".$match[0]);
 		}
-		if ($strict && !empty($notFound)) throw new InvalidArgumentException("Unused variables: ".implode(',', array_keys($notFound)));
+
+		if ($strict && !empty($notFound)) {
+			throw new InvalidArgumentException("Unused variables: ".implode(',', array_keys($notFound)));
+		}
 		return true;
 	}
 
@@ -174,14 +190,14 @@ class SlimPlate {
 	 * @param mixed[] $data
 	 * @return string
 	 */
-	protected function convertVariables (string $text, array $data): string {
+	protected function convertVariables(string $text, array $data): string
+	{
 		foreach ($this->matchVariables() as $match) {
 			if ($text) {
 				if (count($match) === 2 && isset($data[$match[1]])) {
-					$text = preg_replace('/'.preg_quote((string)$match[0]).'/', (string)$data[$match[1]], $text, 1);
-				}
-				else if (count($match) === 3 && isset($data[$match[1]][$match[2]])) {
-					$text = preg_replace('/'.preg_quote((string)$match[0]).'/', (string)$data[$match[1]][$match[2]], $text, 1);
+					$text = preg_replace('/'.preg_quote((string) $match[0]).'/', (string) $data[$match[1]], $text, 1);
+				} elseif (count($match) === 3 && isset($data[$match[1]][$match[2]])) {
+					$text = preg_replace('/'.preg_quote((string) $match[0]).'/', (string) $data[$match[1]][$match[2]], $text, 1);
 				}
 			}
 		}
@@ -194,7 +210,8 @@ class SlimPlate {
 	 * @param string $con
 	 * @return array<int|float|string>|null
 	 */
-	protected function parseUknown (array $data, string $con): ?array {
+	protected function parseUknown(array $data, string $con): ?array
+	{
 		if ($con !== '') {
 			if ($con[0] === '$') return $this->findVar($con, $data);
 			return $this->findValue($con);
@@ -206,8 +223,11 @@ class SlimPlate {
 	 * Find all variables {$var} in template
 	 * @return array<int, array<int, string>>
 	 */
-	protected function matchVariables (): array {
-		if ($this->matchData === null) preg_match_all('/\{\$([a-z][a-z\d_]*)(?:(?:->|-&gt;)([a-z][a-z\d_]*))?\}/iU', $this->template, $this->matchData, PREG_SET_ORDER);
+	protected function matchVariables(): array
+	{
+		if ($this->matchData === null) {
+			preg_match_all('/\{'.self::$regexCond.'?\}/iU', $this->template, $this->matchData, PREG_SET_ORDER);
+		}
 		return $this->matchData;
 	}
 
@@ -218,13 +238,14 @@ class SlimPlate {
 	 * @return string[]
 	 * @throws InvalidArgumentException
 	 */
-	protected function findVar (string $val, array $data): array {
-		preg_match('/^\$([a-z][a-z\d_]*)(?:(?:->|-&gt;)([a-z][a-z\d_]*))?/i', $val, $vars);
+	protected function findVar(string $val, array $data): array
+	{
+		preg_match('/^'.self::$regexCond.'?/i', $val, $vars);
 		if (empty($vars)) throw new InvalidArgumentException("Wrong variable definition");
 
 		if ($data) {
 			if (count($vars) === 2 && isset($data[$vars[1]])) $vars[1] = $data[$vars[1]];
-			else if (count($vars) === 3 && isset($data[$vars[1]][$vars[2]])) $vars[1] = $data[$vars[1]][$vars[2]];
+			elseif (count($vars) === 3 && isset($data[$vars[1]][$vars[2]])) $vars[1] = $data[$vars[1]][$vars[2]];
 			else throw new InvalidArgumentException("Undefined variable: ".$vars[0]);
 		}
 		return $vars;
@@ -235,7 +256,8 @@ class SlimPlate {
 	 * @param string $val
 	 * @return string[]
 	 */
-	protected function findOperator (string $val): array {
+	protected function findOperator(string $val): array
+	{
 		preg_match('/^([=<>]|&gt;|&lt;)+/i', $val, $operator);
 		return $operator;
 	}
@@ -245,11 +267,12 @@ class SlimPlate {
 	 * @param string $val
 	 * @return array<int|float|string>
 	 */
-	protected function findValue (string $val): array {
+	protected function findValue(string $val): array
+	{
 		if ($val[0] === '"') return $this->readValueCon($val, '"');
-		else if ($val[0] === "'") return $this->readValueCon($val, "'");
-		else if (substr($val[0], 0, 6) === '&quot;') return $this->readValueCon($val, '&quot;');
-		else if (substr($val[0], 0, 6) === '&#039;') return $this->readValueCon($val, '&#039;');
+		elseif ($val[0] === "'") return $this->readValueCon($val, "'");
+		elseif (substr($val[0], 0, 6) === '&quot;') return $this->readValueCon($val, '&quot;');
+		elseif (substr($val[0], 0, 6) === '&#039;') return $this->readValueCon($val, '&#039;');
 		return $this->readValueCon($val, null);
 	}
 
@@ -260,13 +283,13 @@ class SlimPlate {
 	 * @return array<int|float|string>
 	 * @throws InvalidArgumentException
 	 */
-	protected function readValueCon (string $val, ?string $sep): array {
+	protected function readValueCon(string $val, ?string $sep): array
+	{
 		if ($sep !== null) {
 			$sep = preg_quote($sep, '/');
 			preg_match('/^'.$sep.'(.*)(?!\\\\'.$sep.').'.$sep.'/iU', $val, $matches);
 			if ($matches) return $matches;
-		}
-		else {
+		} else {
 			preg_match('/( |=|<|>|&lt;|&gt;)/i', $val, $matches);
 			if ($matches) return $this->convertValueCon(substr($val, 0, (int) strpos($val, $matches[0])));
 			return $this->convertValueCon($val);
@@ -279,7 +302,8 @@ class SlimPlate {
 	 * @param string $val
 	 * @return array<int|float|string>
 	 */
-	protected function convertValueCon (string $val): array {
+	protected function convertValueCon(string $val): array
+	{
 		$convert = $val;
 		if (is_numeric($val)) {
 			$convert = filter_var($val, FILTER_VALIDATE_INT) === false ? floatval($val) : intval($val);
@@ -293,7 +317,8 @@ class SlimPlate {
 	 * @param string $val
 	 * @return string
 	 */
-	protected function decideIfElse ($state, string $val): string {
+	protected function decideIfElse($state, string $val): string
+	{
 		$pos = stripos($val, '{else}');
 		if ($pos) {
 			if ($state) return substr($val, 0, $pos);
@@ -310,8 +335,9 @@ class SlimPlate {
 	 * @param mixed $y
 	 * @return bool|null
 	 */
-	protected function operator ($x, string $operator, $y): ?bool {
-		switch (str_replace(['&lt;','&gt;'], ['<','>'], $operator)) {
+	protected function operator($x, string $operator, $y): ?bool
+	{
+		switch (str_replace(['&lt;', '&gt;'], ['<', '>'], $operator)) {
 			case '==': return $x == $y;
 			case '===': return $x === $y;
 			case '<>':
@@ -331,7 +357,8 @@ class SlimPlate {
 	 * @param string $regex
 	 * @return string
 	 */
-	protected function removeRegex (string $text, string $regex): string {
+	protected function removeRegex(string $text, string $regex): string
+	{
 		$out = preg_replace('/'.preg_quote($regex, '/').'/', '', $text, 1);
 		return $out === null ? '' : trim($out);
 	}
--- tests/SlimPlateTest.php
+++ tests/SlimPlateTest.php
@@ -1,18 +1,21 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 use SlimPlate\SlimPlate;
-
 use PHPUnit\Framework\TestCase;
 
-class SlimPlateTest extends TestCase {
+class SlimPlateTest extends TestCase
+{
 
-	public function testIfValue (): void {
+	public function testIfValue(): void
+	{
 		$t = new SlimPlate('{if 5}OK{/if}');
 		$this->assertTrue($t->check([]));
 		$this->assertEquals($t->render([]), 'OK');
 	}
 
-	public function testIfValueFail (): void {
+	public function testIfValueFail(): void
+	{
 		$t = new SlimPlate('{if}OK{/if}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Empty IF condition');
@@ -21,149 +24,170 @@ class SlimPlateTest extends TestCase {
 		$this->assertEquals($t->render([]), '{if}OK{/if}');
 	}
 
-	public function testIfElse (): void {
+	public function testIfElse(): void
+	{
 		$t = new SlimPlate('{if 5}OK{else}wrong{/if} {if 0}wrong{else}OK{/if}');
 		$this->assertEquals($t->check([]), true);
 		$this->assertEquals($t->render([]), 'OK OK');
 	}
 
-	public function testIfVariable (): void {
+	public function testIfVariable(): void
+	{
 		$t = new SlimPlate('{if $x}OK{/if}');
-		$this->assertEquals($t->check(['x'=>1]), true);
-		$this->assertEquals($t->render(['x'=>1]), 'OK');
-		$this->assertEquals($t->render(['x'=>'']), '');
-		$this->assertEquals($t->render(['x'=>0]), '');
+		$this->assertEquals($t->check(['x' => 1]), true);
+		$this->assertEquals($t->render(['x' => 1]), 'OK');
+		$this->assertEquals($t->render(['x' => '']), '');
+		$this->assertEquals($t->render(['x' => 0]), '');
 
 		$t2 = new SlimPlate('{if $x->y}OK{/if}');
-		$this->assertEquals($t2->check(['x'=>['y'=>1]]), true);
-		$this->assertEquals($t2->render(['x'=>['y'=>1]]), 'OK');
+		$this->assertEquals($t2->check(['x' => ['y' => 1]]), true);
+		$this->assertEquals($t2->render(['x' => ['y' => 1]]), 'OK');
 	}
 
-	public function testIfVariableVariable (): void {
+	public function testIfVariableVariable(): void
+	{
 		$t = new SlimPlate('{if 5 == 5}OK{/if}{if 6.1 == 5}Wrong{/if}');
 		$this->assertEquals($t->check([]), true);
 		$this->assertEquals($t->render([]), 'OK');
 	}
 
-	public function testIfStringValue (): void {
+	public function testIfStringValue(): void
+	{
 		$t = new SlimPlate('{if "5" == \'5\'}OK{/if}{if &quot;6&quot; == &#039;5&#039;}Wrong{/if}');
 		$this->assertEquals($t->check([]), true);
 		$this->assertEquals($t->render([]), 'OK');
 	}
 
-	public function testIfOperators (): void {
+	public function testIfOperators(): void
+	{
 		$t = new SlimPlate('{if $x == $y}=={/if}{if $x === $y}==={/if}{if $x > $y}>{/if}{if $x >= $y}>={/if}{if $x < $y}<{/if}{if $x <= $y}<={/if}');
-		$this->assertEquals($t->check(['x'=>1,'y'=>1]), true);
-		$this->assertEquals($t->render(['x'=>1,'y'=>1]), '=====>=<=');
-		$this->assertEquals($t->render(['x'=>'1','y'=>1]), '==>=<=');
-		$this->assertEquals($t->render(['x'=>'2','y'=>1]), '>>=');
-		$this->assertEquals($t->render(['x'=>1,'y'=>2]), '<<=');
+		$this->assertEquals($t->check(['x' => 1, 'y' => 1]), true);
+		$this->assertEquals($t->render(['x' => 1, 'y' => 1]), '=====>=<=');
+		$this->assertEquals($t->render(['x' => '1', 'y' => 1]), '==>=<=');
+		$this->assertEquals($t->render(['x' => '2', 'y' => 1]), '>>=');
+		$this->assertEquals($t->render(['x' => 1, 'y' => 2]), '<<=');
 	}
 
-	public function testIfEscOperators (): void {
+	public function testIfEscOperators(): void
+	{
 		$t = new SlimPlate('{if $x &gt; $y}>{/if}{if $x &gt;= $y}>={/if}{if $x &lt; $y}<{/if}{if $x &lt;= $y}<={/if}');
-		$this->assertEquals($t->check(['x'=>1,'y'=>1]), true);
-		$this->assertEquals($t->render(['x'=>1,'y'=>1]), '>=<=');
-		$this->assertEquals($t->render(['x'=>'2','y'=>1]), '>>=');
-		$this->assertEquals($t->render(['x'=>1,'y'=>2]), '<<=');
+		$this->assertEquals($t->check(['x' => 1, 'y' => 1]), true);
+		$this->assertEquals($t->render(['x' => 1, 'y' => 1]), '>=<=');
+		$this->assertEquals($t->render(['x' => '2', 'y' => 1]), '>>=');
+		$this->assertEquals($t->render(['x' => 1, 'y' => 2]), '<<=');
 	}
 
-	public function testValues (): void {
+	public function testValues(): void
+	{
 		$t = new SlimPlate('{$a} a {$b->a} a {$b-&gt;c}');
-		$data = ['a'=>1, 'b'=>['a'=>2, 'c'=>3]];
+		$data = ['a' => 1, 'b' => ['a' => 2, 'c' => 3]];
 		$t->check($data, true);
 		$this->assertEquals('1 a 2 a 3', $t->render($data));
 	}
 
-	public function testRenderBadTemplate (): void {
+	public function testRenderBadTemplate(): void
+	{
 		$t = new SlimPlate("{if 6 > \"x'}OK{/if}");
 		$this->assertEquals('&#123;if 6 > "x\'}OK{/if&#125;', $t->render());
 	}
 
-	public function testRenderBadTemplateWithVariable (): void {
+	public function testRenderBadTemplateWithVariable(): void
+	{
 		$t = new SlimPlate('{if x www}{$a}{/if}');
-		$this->assertEquals('&#123;if x www}1{/if&#125;', $t->render(['a'=>1]));
+		$this->assertEquals('&#123;if x www}1{/if&#125;', $t->render(['a' => 1]));
 	}
 
-	public function testElseFail (): void {
+	public function testElseFail(): void
+	{
 		$t = new SlimPlate('{if 1}a{else}b{else}c{/if}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Multiple else in: a{else}b{else}c');
 		$t->check([]);
 	}
 
-	public function testBadOpreatorFail (): void {
+	public function testBadOpreatorFail(): void
+	{
 		$t = new SlimPlate("{if x ==== y}aaaa{/if}");
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Undefined IF operator: ==== y');
 		$t->check([]);
 	}
 
-	public function testNoOperatorFail (): void {
+	public function testNoOperatorFail(): void
+	{
 		$t = new SlimPlate('{if x www}aaaa{/if}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Undefined IF operator: www');
 		$t->check([]);
 	}
 
-	public function testBadValueFail (): void {
+	public function testBadValueFail(): void
+	{
 		$t = new SlimPlate("{if 6 > \"x'}OK{/if}");
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Undefined value: ');
 		$t->check();
 	}
 
-	public function testRenderPassWithError (): void {
+	public function testRenderPassWithError(): void
+	{
 		$t = new SlimPlate("{if 6 > \"x'}OK{/if}");
 		$this->assertEquals('&#123;if 6 > "x\'}OK{/if&#125;', $t->render());
 	}
 
-	public function testVariableFail (): void {
+	public function testVariableFail(): void
+	{
 		$t = new SlimPlate('{if $x}aaaa{/if}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Undefined variable: $x');
-		$t->check(['w'=>1]);
+		$t->check(['w' => 1]);
 	}
 
-	public function testNoVariableFail (): void {
+	public function testNoVariableFail(): void
+	{
 		$t = new SlimPlate('{if x ===}aaaa{/if}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Missing second variable: x ===');
 		$t->check([]);
 	}
 
-	public function testNameVariableFail (): void {
+	public function testNameVariableFail(): void
+	{
 		$t = new SlimPlate('{if $_a}y{/if}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Wrong variable definition');
-		$t->check(['y'=>'a']);
+		$t->check(['y' => 'a']);
 	}
 
-	public function testMissingVariableFail (): void {
+	public function testMissingVariableFail(): void
+	{
 		$t = new SlimPlate('{$a}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Unset variable: {$a}');
-		$t->check(['y'=>'a']);
+		$t->check(['y' => 'a']);
 	}
 
-	public function testCheckStrickValuesFail (): void {
+	public function testCheckStrickValuesFail(): void
+	{
 		$t = new SlimPlate('{$a}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Unused variables: b,c');
-		$t->check(['a'=>'a','b'=>'b','c'=>'c'], true);
+		$t->check(['a' => 'a', 'b' => 'b', 'c' => 'c'], true);
 	}
 
-	public function testCheckWrongValueTypeFail (): void {
+	public function testCheckWrongValueTypeFail(): void
+	{
 		$t = new SlimPlate('{$a->a}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Unset variable: {$a->a}');
-		$t->check(['a'=>['b'=>'b']]);
+		$t->check(['a' => ['b' => 'b']]);
 	}
 
-	public function testCheckWrongValueArrayTypeFail (): void {
+	public function testCheckWrongValueArrayTypeFail(): void
+	{
 		$t = new SlimPlate('{$a->a}');
 		$this->expectException('InvalidArgumentException');
 		$this->expectExceptionMessage('Unused variables: a');
-		$t->check(['a'=>['a'=>'a','b'=>'b','c'=>'c']], true);
+		$t->check(['a' => ['a' => 'a', 'b' => 'b', 'c' => 'c']], true);
 	}
 }
